4a0567e26691082b0c242dd61017498e228bd4d6
cabeca1143 <72713387+cabeca1143@users.noreply.github.com>
2021-06-01 10:49:13 -0300

    Changes I made to my Jan Server but I forgot to apply here
    Added Katarina QWE
    Added Kassadin ER
    Added DrMundo PassiveQWER
    Added Graves Q

---
+++ Content/LeagueSandbox-Scripts/Buffs/DrMundo/RBuff.cs
@@ -0,0 +1,34 @@
+using GameServerCore.Enums;
+using GameServerCore.Domain.GameObjects;
+using LeagueSandbox.GameServer.GameObjects.Stats;
+using GameServerCore.Domain.GameObjects.Spell;
+using GameServerCore.Scripting.CSharp;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+
+
+namespace Sadism
+{
+    internal class Sadism : IBuffGameScript
+    {
+        public BuffType BuffType => BuffType.HASTE;
+        public BuffAddType BuffAddType => BuffAddType.REPLACE_EXISTING;
+        public int MaxStacks => 1;
+        public bool IsHidden => false;
+
+        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            StatsModifier.MoveSpeed.PercentBonus += 0.05f + (0.1f * ownerSpell.CastInfo.SpellLevel);
+            StatsModifier.HealthRegeneration.PercentBonus += unit.Stats.HealthPoints.Total * (0.0033f + (0.00084f * (ownerSpell.CastInfo.SpellLevel - 1))); //Double check these numbers. They look right mathematically, but look wrong in game...
+            unit.AddStatModifier(StatsModifier);
+        }
+
+        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+        }
+        public void OnUpdate(float diff)
+        {
+        }
+    }
+}
